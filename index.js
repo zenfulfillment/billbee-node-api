5834f5cb9ef440da268609789be4d8d46c607b18
dubkrapht <joanpapajani47@gmail.com>
2024-11-06 12:26:04 +0100

    feat(*): replace request w axios

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Promise = require('bluebird');
-const request = require('request-promise');
+const axios = require('axios');
 const _ = require('lodash');
 
 const rejectMissingUrl = () => Promise.reject(new Error('Missing url'));
@@ -21,48 +21,49 @@ module.exports = ({apiKey = '', user = '', pass = '', version = 'v1'} = {}, {str
   }
 
   function maybeParse(res) {
-    return stringifyBigInt ? JSON.parse(bigIntStringify(res)) : res;
+    return stringifyBigInt ? JSON.parse(bigIntStringify(res.data)) : res;
   }
 
   async function delayIfLimitReached(err, fn) {
-    if (err.statusCode === 429) {
+    if (err.response && err.response.status === 429) {
       await Promise.delay(2500).then(() => fn());
     }
     throw err;
   }
 
-  function _request(args) {
-    return request.defaults({
-      baseUrl: `https://app.billbee.io/api/${version}`,
-      json: stringifyBigInt ? false : true,
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8',
-        'X-Billbee-Api-Key': apiKey,
-      },
-      auth: {
-        user,
-        pass
-      }
-    })(args).promise();
+  const axiosInstance = axios.create({
+    baseURL: `https://app.billbee.io/api/${version}`,
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      'X-Billbee-Api-Key': apiKey,
+    },
+    auth: {
+      username: user,
+      password: pass,
+    },
+  });
+
+  function _request(config) {
+    return axiosInstance(config);
   }
 
   return {
-    get(url, qs = {}) {
+    get(url, params = {}) {
       if (_.isEmpty(url)) {
         return rejectMissingUrl();
       }
-      return _request({url, method: 'GET', qs})
+      return _request({url, method: 'GET', params})
         .then(maybeParse)
         .catch((err) => {
           return Promise.resolve(delayIfLimitReached(err, () => _request({
             url,
             method: 'GET',
-            qs
+            params
           })));
         });
     },
 
-    post(url, body) {
+    post(url, data) {
       if (_.isEmpty(url)) {
         return rejectMissingUrl();
       }
@@ -71,45 +72,42 @@ module.exports = ({apiKey = '', user = '', pass = '', version = 'v1'} = {}, {str
         return rejectMissingBody();
       }
 
-      return _request({url, method: 'POST', body, json: true})
+      return _request({url, method: 'POST', data})
         .catch((err) => {
           return Promise.resolve(delayIfLimitReached(err, () => _request({
             url,
             method: 'POST',
-            body,
-            json: true
+            data
           })));
         });
     },
 
-    put(url, body = {}) {
+    put(url, data = {}) {
       if (_.isEmpty(url)) {
         return rejectMissingUrl();
       }
 
-      return _request({url, method: 'PUT', body, json: true})
+      return _request({url, method: 'PUT', data})
         .catch((err) => {
           return Promise.resolve(delayIfLimitReached(err, () => _request({
             url,
             method: 'POST',
-            body,
-            json: true
+            data
           })));
         });
     },
 
-    patch(url, body = {}) {
+    patch(url, data = {}) {
       if (_.isEmpty(url)) {
         return rejectMissingUrl();
       }
 
-      return _request({url, method: 'PATCH', body, json: true})
+      return _request({url, method: 'PATCH', data})
         .catch((err) => {
           return Promise.resolve(delayIfLimitReached(err, () => _request({
             url,
             method: 'PATCH',
-            body,
-            json: true
+            data
           })));
         });
     },
